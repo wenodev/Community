83f38c992ae46b845d3fb4e1beb5dc24b645a4d3
weno <logicalstark1105@gmail.com>
2021-06-22 23:07:26 +0900

    feat: 8장 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import InputSample from './InputSample';
 
 function App() {
+
+  const style = {
+    marginTop : 100,
+    padding : 10,
+    textAlign : "center"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <InputSample/>
     </div>
   );
 }
