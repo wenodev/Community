5394486642ef34fce493a2d975f6114421b723e1
weno <logicalstark1105@gmail.com>
2021-07-05 22:59:29 +0900

    feat: 1-12, 1-13 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React, { useState, useRef, useEffect } from 'react';
 import UserList from './UserList';
-
-
-
+import CreateUser from './CreateUser';
 
 function App() {
+
+  const [inputs, setIntpus] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setIntpus({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+    const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+    useEffect(() => {
+    console.log("nextId: ", nextId);
+  }, )
+
+
+  const onCreate = () => {
+    const user = {
+      id : nextId.current,
+      username,
+      email
+    }
+
+    setUsers([...users, user]);
+
+    setIntpus({
+      username: '',
+      email: ''
+    })
+    nextId.current += 1;
+  }
 
   const style = {
     marginTop : 100,
@@ -16,7 +69,8 @@ function App() {
   return (
     <div style={style}>
       {/* <InputSample/> */}
-      <UserList />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users}/>      
     </div>
   );
 }
