b94676e85ae2349fd766187e22d7affe55ef8410
wenodev <logicalstark1105@gmail.com>
2021-07-08 20:34:44 +0900

    feat: 1-15( boolean 으로 글자색 변경하라)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -20,29 +20,27 @@ function App() {
   }
 
     const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
+      {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com',
+        active: true
+      },
+      {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com',
+        active: false
+      },
+      {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com',
+        active: false
+      }
   ]);
 
   const nextId = useRef(4);
-
-    useEffect(() => {
-    console.log("nextId: ", nextId);
-  }, )
-
 
   const onCreate = () => {
     const user = {
@@ -50,8 +48,7 @@ function App() {
       username,
       email
     }
-
-    setUsers([...users, user]);
+    setUsers(users.concat(user));
 
     setIntpus({
       username: '',
@@ -64,17 +61,26 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id === id ? {...user, active: !user.active} : user
+        )
+    );
+    
+  }
+
   const style = {
-    marginTop : 100,
+    margin : 100,
     padding : 10,
-    textAlign : "center"
+    textAlign : "left"
   }
 
   return (
     <div style={style}>
       {/* <InputSample/> */}
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} onRemove={onRemove} />      
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />      
     </div>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
-function User({user, onRemove}){
+function User({user, onRemove, onToggle}){
     return(
         <div>
-            id: {user.id}, username: {user.username},  emil: {user.email}            
+            <b
+                style={
+                    {
+                        cursor: 'pointer',
+                        color : user.active ? 'green' : 'black',
+                    }
+                }
+                onClick={() => onToggle(user.id)}
+                >
+                {user.username}  
+            </b>,
+            <span>{user.email}</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users, onRemove}){
+function UserList({users, onRemove, onToggle}){
   return (
       <div>
         {users.map((user, index) => 
-            <User user={user} key={user.id} onRemove={onRemove} />
+            <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
         )}
       </div>
   )
